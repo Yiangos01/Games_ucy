988b4f19383aaf1ac2f44ab5b4133f9c46814da6
Yiangos01 <ygeorgiou12@gmail.com>
2021-03-25 22:49:48 +0200

    Player and recurrent road spawning added

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // void Start()
+    // {
+
+    // }
+
+    public float speed = 5;
+    public Rigidbody rb;
+    public float horizontalMultiplier = 2;
+
+    float horizontalInput;
+
+    private void FixedUpdate()
+    {
+        Vector3 forwardMove = transform.forward * speed * Time.fixedDeltaTime;
+        Vector3 horizontalMove = transform.right * horizontalInput * speed * Time.fixedDeltaTime; 
+        rb.MovePosition(rb.position + forwardMove + horizontalMove);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+    }
+}

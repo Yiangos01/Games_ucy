e9ecaf9012c867a089d5366f5e16ee857fb91a0b
Yiangos01 <ygeorgiou12@gmail.com>
2021-04-18 21:57:54 +0300

    Fog, terrain, win and gameover messages, Strength mode color change and set to true after completing the pattern condition

--- Assets/Scripts/ChickenMovement.cs
+++ Assets/Scripts/ChickenMovement.cs
@@ -14,6 +14,12 @@ public class ChickenMovement : MonoBehaviour
     protected Animator animator;
     protected ChickenStatus chSt;
     protected Vector3 vel = Vector3.zero;
+    public GameObject terrain;
+    public GameObject chicken;
+    public GameObject uiFinish;
+    private bool canMove = true;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +28,7 @@ public class ChickenMovement : MonoBehaviour
         col = GetComponent<SphereCollider>();
         animator = transform.GetChild(0).gameObject.GetComponent<Animator>(); //GetComponent<Animator>();
         chSt = GetComponent<ChickenStatus>();
+        
     }
 
     //Check if the player is grounded.
@@ -30,7 +37,17 @@ public class ChickenMovement : MonoBehaviour
         return Physics.CheckSphere(transform.position, 0.2f, groundLayers, QueryTriggerInteraction.Ignore);
     }
 
-    void OnTriggerStay(Collider other)
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Barn"))
+        {
+
+            uiFinish.SetActive(true);
+            canMove = false;
+
+        }
+    }
+        void OnTriggerStay(Collider other)
     {
 
         if (other.gameObject.tag == "Left")
@@ -55,57 +72,65 @@ public class ChickenMovement : MonoBehaviour
    
     void FixedUpdate()
     {
-
-        float hmove = Input.GetAxis("Horizontal");
-        //float vmove = Input.GetAxis("Vertical");
-        //vmove = Mathf.Clamp01(vmove);
-        float vmove = 1.0f;
-        Camera cam = Camera.main;
-        Vector3 forward = cam.transform.forward;
-        forward.y = 0;
-        forward.Normalize();
-
-        Vector3 right = cam.transform.right;
-        right.y = 0;
-        right.Normalize();
-
-        if (vmove != 0)
+        if (canMove)
         {
-            if (IsGrounded() && !chSt.isHit)
+            float hmove = Input.GetAxis("Horizontal");
+            //float vmove = Input.GetAxis("Vertical");
+            //vmove = Mathf.Clamp01(vmove);
+            float vmove = 1.0f;
+            Camera cam = Camera.main;
+            Vector3 forward = cam.transform.forward;
+            forward.y = 0;
+            forward.Normalize();
+
+            Vector3 right = cam.transform.right;
+            right.y = 0;
+            right.Normalize();
+
+            if (vmove != 0)
             {
-                // Vector3 move = hmove * transfom.right + vmove * forward;//One of the two, don't know which one is better
-                Vector3 move = hmove * right + vmove * forward;
-                rb.MovePosition(rb.position + move * Time.fixedDeltaTime * moveSpeed);
-                transform.GetChild(0).gameObject.transform.rotation = Quaternion.LookRotation(move);//change direction of chicken mesh according with the movement(go right-left)
+                if (IsGrounded() && !chSt.isHit)
+                {
+                    // Vector3 move = hmove * transfom.right + vmove * forward;//One of the two, don't know which one is better
+                    Vector3 move = hmove * right + vmove * forward;
+                    rb.MovePosition(rb.position + move * Time.fixedDeltaTime * moveSpeed);
+                    transform.GetChild(0).gameObject.transform.rotation = Quaternion.LookRotation(move);//change direction of chicken mesh according with the movement(go right-left)
+                }
+                else if (!IsGrounded())
+                {
+                    Vector3 move = hmove * right + vmove * forward;
+                    rb.MovePosition(rb.position + move * Time.fixedDeltaTime * moveSpeed);
+                    transform.GetChild(0).gameObject.transform.rotation = Quaternion.LookRotation(move);//change direction of chicken mesh according with the movement(go right-left)
+                }
             }
-            else if (!IsGrounded())
-            {
-                Vector3 move = hmove * right+ vmove * forward;
-                rb.MovePosition(rb.position + move * Time.fixedDeltaTime * moveSpeed);
-                transform.GetChild(0).gameObject.transform.rotation = Quaternion.LookRotation(move);//change direction of chicken mesh according with the movement(go right-left)
-            }
+            //Set Run Animation
+            animator.SetFloat("ChickenSpeed", vmove);
+            //Set part of Jump animation
+            animator.SetBool("IsGrounded", IsGrounded());
         }
-        //Set Run Animation
-        animator.SetFloat("ChickenSpeed", vmove);
-        //Set part of Jump animation
-        animator.SetBool("IsGrounded", IsGrounded());
-        
- 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space) && !chSt.isHit)
-        {   //Jump Force
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            //Set part of Jump animation
-            animator.SetTrigger("Jump");
-           
+        if (canMove)
+        {
+            if (IsGrounded() && Input.GetKeyDown(KeyCode.Space) && !chSt.isHit)
+            {   //Jump Force
+                rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+                //Set part of Jump animation
+                animator.SetTrigger("Jump");
+
+            }
+            isgrounded = IsGrounded();
+
+            // Move terrain based on player movement
+            Vector3 terrain_pos = chicken.transform.position;
+            terrain_pos[1] = -2;
+            terrain_pos[0] = terrain_pos[0] - 500;
+            terrain_pos[2] = terrain_pos[2] - 500;
+            terrain.transform.position = terrain_pos;
         }
-        isgrounded = IsGrounded();
 
     }
-
-
 }
